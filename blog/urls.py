ed26fc674a7289e9a038d52e30a8ebb18baad265
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-12 18:17:59 +0530

    Ce

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='blogindex'),
     url(r'^new/$', views.new, name='new'),
 
 ]
