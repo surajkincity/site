07830ade01602ebad2927bdda8c8e14645370f9d
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-12 19:47:16 +0530

    Wx

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,11 @@ import datetime
 class blog(models.Model):
     title = models.CharField(max_length=5000)
     body = models.TextField()
-    date = models.DateField(default = datetime.datetime.now())
+    date = models.DateField(default = django.utils.timezone.now())
 
 
 
 class comment(models.Model):
     blog = models.CharField(max_length=5000)
     comment = models.TextField()
-    date = models.DateField(default = datetime.datetime.now())
+    date = models.DateField(default = django.utils.timezone.now())
