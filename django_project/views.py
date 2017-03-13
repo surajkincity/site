b335dc22129a65cc3ad30c6c04bc1ea7f5e1df89
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-13 09:26:38 +0530

    Cd

--- django_project/views.py
+++ django_project/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', )
+    return render(request, 'index.html', )
 
 def about(request):
     return render(request, 'about.html', )
