d41471e345820bdd756a4520236332e2390769bc
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-19 19:22:26 +0530

    D dd

--- django_project/staticfiles/index.js
+++ django_project/staticfiles/index.js
@@ -1,3 +1,24 @@
 tinymce.init({
-   selector: '#mytextarea'
- });
+  selector: 'textarea',
+  height: 500,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste code'
+  ],
+  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  content_css: [
+    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+    '//www.tinymce.com/css/codepen.min.css'
+  ]
+});
+
+$("button").click(function(){
+  var template = tinyMCE.activeEditor.getContent({format : 'raw'});
+  console.log(template);
+                    $("#show-me").text(template);
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+                   });
+
