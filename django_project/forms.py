159c5907df7f28d3081dc13a6edfa5ae02fc841d
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-30 12:10:49 +0530

    Cece

--- django_project/forms.py
+++ django_project/forms.py
@@ -5,14 +5,14 @@ from .models import newsletter,leads,contact
 class contactform(forms.ModelForm):
     class Meta:
         model = contact
-        fields = ('message','email')
+        fields = ('message','email',)
 
 class leadsform(forms.ModelForm):
     class Meta:
         model = leads
-        fields = ('website','email')
+        fields = ('website','email',)
 
 class newsletterform(forms.ModelForm):
     class Meta:
         model = newsletter
-        fields = ('email')
+        fields = ('email',)
