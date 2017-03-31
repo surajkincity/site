6b4b836bce9b18d20efbc1c5bfd4f7303982b38e
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-31 11:56:24 +0530

    Cece

--- django_project/views.py
+++ django_project/views.py
@@ -4,52 +4,53 @@ from .forms import contactform,newsletterform,leadsform
 
 
 def home(request):
-    if request.method == "POST":
-    	form = leadsform(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            message = 'Thanks! We will get back to you shortly.'
-            form = ' '
-            display = 'none'
-            return render(request, 'index.html', {'form': form, 'display' :display , 'message' :message})
-        
-    else:
-    	message = ''
-    	display = 'block'
-        form = leadsform()
-        return render(request, 'index.html', {'form': form, 'display' :display , 'message' :message})
+	if request.method == "POST":
+		form = leadsform(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.save()
+			message = 'Thanks! We will get back to you shortly.'
+			form = ' '
+			display = 'none'
+			return render(request, 'index.html', {'form': form, 'display' :display , 'message' :message})
+		
+	else:
+		message = ''
+		display = 'block'
+		form = leadsform()
+		return render(request, 'index.html', {'form': form, 'display' :display , 'message' :message})
 
 def newsletter(request):
 	if request.method == "POST":
 		form = newsletterform(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            message = 'Suscribed'
-            form = ' '
-            display = 'none'
-            return render(request, 'newsletter.html', {'form': form, 'display' :display , 'message' :message})
-    
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.save()
+			message = 'Suscribed'
+			form = ' '
+			display = 'none'
+			return render(request, 'newsletter.html', {'form': form, 'display' :display , 'message' :message})
+		else:
+			return render(request, 'newsletter.html', {'form': form, 'display' :display , 'message' :message})
     else:
-    	message = ''
-    	display = 'block'
-        form = newsletterform()
-        return render(request, 'newsletter.html', {'form': form, 'display' :display , 'message' :message})
+		message = ''
+		display = 'block'
+		form = newsletterform()
+		return render(request, 'newsletter.html', {'form': form, 'display' :display , 'message' :message})
 
 
 
 def about(request):
-    return render(request, 'about.html', )
+	return render(request, 'about.html', )
 
 def contact(request):
-    return render(request, 'contact.html',  )
+	return render(request, 'contact.html',  )
 
 def privacy(request):
-    return render(request, 'privacy.html', )
+	return render(request, 'privacy.html', )
 
 def sitemap(request):
-    return render(request, 'sitemap.xml')
+	return render(request, 'sitemap.xml')
 
 def google(request):
-    return render(request, 'googlefa975541a78f7cc0.html')
+	return render(request, 'googlefa975541a78f7cc0.html')
