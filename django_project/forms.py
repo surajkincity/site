39927f5f7116951b16740231ec3021ced3ae5816
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-04-02 09:31:04 +0530

    Sdv

--- django_project/forms.py
+++ django_project/forms.py
@@ -11,8 +11,12 @@ class leadsform(forms.ModelForm):
     class Meta:
         model = leads
         fields = ('website','email',)
+        help_texts = {
+            'website': 'Website', 'email': 'Email',
+        }
 
 class newsletterform(forms.ModelForm):
     class Meta:
         model = newsletter
         fields = ('email',)
+
