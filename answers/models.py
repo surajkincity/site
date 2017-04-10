b80a8199863491cfbf44a5e65e1c726b32c73ce9
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-04-10 19:10:45 +0530

    Assad

--- answers/models.py
+++ answers/models.py
@@ -6,7 +6,7 @@ import datetime
 class blog(models.Model):
     title = models.CharField(max_length=5000)
     body = models.TextField()
-    date = models.DateField(default = datetime.datetime.now())
+    date = models.DateField(default = datetime.datetime.now,blank=True)
     name = models.CharField(max_length=5000)
     def slug(self):
         slugit= self.title.replace(' ', '-').replace('?', '').replace('@', '*')
@@ -23,7 +23,7 @@ class comment(models.Model):
     name = models.CharField(max_length=5000)
     blog = models.CharField(max_length=5000,null=True,blank=True)
     comment = models.TextField()
-    date = models.DateField(default = datetime.datetime.now(),blank=True)
+    date = models.DateField(default = datetime.datetime.now,blank=True)
 
     def ic(self):
         icit = self.name[0]
